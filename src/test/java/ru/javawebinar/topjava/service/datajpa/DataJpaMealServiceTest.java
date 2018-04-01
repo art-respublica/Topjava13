0fad1b46d4f8b1270c74a22b32f7f4fe7e3b55a7
respublica <respublica_news@ukr.net>
2018-04-01 21:18:21 +0300

    HW5: optional - get user with meals and get meal with user

--- src/test/java/ru/javawebinar/topjava/service/datajpa/DataJpaMealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/datajpa/DataJpaMealServiceTest.java
@@ -1,10 +1,29 @@
 package ru.javawebinar.topjava.service.datajpa;
 
+import org.junit.Test;
 import org.springframework.test.context.ActiveProfiles;
+import ru.javawebinar.topjava.UserTestData;
+import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.service.MealServiceTest;
+import ru.javawebinar.topjava.util.exception.NotFoundException;
 
+import static ru.javawebinar.topjava.MealTestData.*;
+import static ru.javawebinar.topjava.UserTestData.ADMIN_ID;
 import static ru.javawebinar.topjava.Profiles.DATAJPA;
 
 @ActiveProfiles(DATAJPA)
 public class DataJpaMealServiceTest extends MealServiceTest {
+
+    @Test
+    public void getWithUser() throws Exception {
+        Meal adminMeal = service.getWithUser(ADMIN_MEAL_ID, ADMIN_ID);
+        assertMatch(ADMIN_MEAL1, adminMeal);
+        UserTestData.assertMatch(UserTestData.ADMIN, adminMeal.getUser());
+    }
+
+    @Test
+    public void getWithUserNotFound() throws Exception {
+        thrown.expect(NotFoundException.class);
+        service.getWithUser(MEAL1_ID, ADMIN_ID);
+    }
 }
--- src/test/java/ru/javawebinar/topjava/service/datajpa/DataJpaUserServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/datajpa/DataJpaUserServiceTest.java
@@ -1,10 +1,28 @@
 package ru.javawebinar.topjava.service.datajpa;
 
+import org.junit.Test;
 import org.springframework.test.context.ActiveProfiles;
+import ru.javawebinar.topjava.MealTestData;
+import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.service.UserServiceTest;
+import ru.javawebinar.topjava.util.exception.NotFoundException;
 
 import static ru.javawebinar.topjava.Profiles.DATAJPA;
+import static ru.javawebinar.topjava.UserTestData.*;
 
 @ActiveProfiles(DATAJPA)
 public class DataJpaUserServiceTest extends UserServiceTest {
+
+    @Test
+    public void getWithMeals() throws Exception {
+        User user = service.getWithMeals(USER_ID);
+        assertMatch(USER, user);
+        MealTestData.assertMatch(MealTestData.MEALS, user.getMeals());
+    }
+
+    @Test
+    public void getWithMealsNotFound() throws Exception {
+        thrown.expect(NotFoundException.class);
+        service.getWithMeals(1);
+    }
 }
