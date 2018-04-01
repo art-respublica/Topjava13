4565ac021dab820773078736fe8a760938092938
respublica <respublica_news@ukr.net>
2018-04-01 19:59:35 +0300

    HW5: optional - divided JdbcMealRepositoryImpl into HSQLDB and Postgres

---
+++ src/main/java/ru/javawebinar/topjava/repository/jdbc/Java8JdbcMealRepositoryImpl.java
@@ -0,0 +1,26 @@
+package ru.javawebinar.topjava.repository.jdbc;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
+import ru.javawebinar.topjava.Profiles;
+
+import javax.sql.DataSource;
+import java.time.LocalDateTime;
+
+@Repository
+@Profile(Profiles.POSTGRES_DB)
+public class Java8JdbcMealRepositoryImpl extends JdbcMealRepositoryImpl<LocalDateTime> {
+
+    @Autowired
+    public Java8JdbcMealRepositoryImpl(DataSource dataSource, JdbcTemplate jdbcTemplate, NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
+        super(dataSource, jdbcTemplate, namedParameterJdbcTemplate);
+    }
+
+    @Override
+    protected LocalDateTime toDbDateTime(LocalDateTime ldt) {
+        return ldt;
+    }
+}
